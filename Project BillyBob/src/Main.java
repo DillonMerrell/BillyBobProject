804f14db58aca41a8bc5057a8ca0faa5debfb292
DillonMerrell <wafflemayhemofficial@gmail.com>
2017-11-27 09:29:17 -0600

    Add files via upload

--- Project BillyBob/src/Main.java
+++ Project BillyBob/src/Main.java
@@ -27,11 +27,14 @@ public class Main
 		GAME:
 			while(running)
 			{
-				System.out.println("Time: Some time in the future.");
+				System.out.println("\tChapter 1");
+				System.out.println("Time: Present Day");
 				System.out.println("------------------------------------");
 				System.out.println("You come home from a long days work. You sit down at\n"
 						+ "the kitchen table and your wife \"Patricia\" looks somber.\n"
 						+ "She looks up at you with a tear in her eye and murmurs\n");
+				waitWords();
+				waitWords();
 				waitWords();
 				System.out.println("\tPatricia: Your father...");
 				waitWords();
@@ -45,6 +48,7 @@ public class Main
 				waitWords();
 				System.out.println("\tPatricia: He left you this.\n");
 				waitWords();
+				promptEnterKey();
 				System.out.println("She slides you a notecard written by your father.");
 				System.out.println("It reads: ");
 				System.out.println("---------------------------------------------------------");
@@ -59,10 +63,42 @@ public class Main
 				System.out.println("|                                                        |");
 				System.out.println("|                                                        |");
 				System.out.println("|                                                        |");
-				System.out.println("---------------------------------------------------------\n\n\n");
+				System.out.println("---------------------------------------------------------\n");
+				System.out.println("You place the notecard down on the table and leave to your fathers house.\n");
+				promptEnterKey();
+				clearScreen();
+				boolean decision1 = true;
 				
-				System.out.println("You place the notecard down on the table.");
-				
+				while(decision1 == true)
+				{
+					System.out.println("\tChapter 2");
+					System.out.println("You enter your fathers basement and look around the room.\n"
+							+ "You notice a glowing box on the table. Do you open it?\n");
+					String dec1 = input.nextLine();
+					if(dec1.equals("yes") || dec1.equals("Yes"))
+					{
+						decision1 = false;
+						System.out.println("You open the box and you see a shiney button with some dials set to \"December 7, 1970\"\n"
+								+ "You press the button. It makes noises and knocks you off your feet. You look around and nothing seems\n"
+								+ "to have changed. You hear footsteps coming from upstairs...\n");
+						break;
+					}
+					else if(dec1.equals("no")|| dec1.equals("No"))
+					{
+						System.out.println("You go home and die at the age of 78.\n"
+								+ "THE END");
+								promptEnterKey();
+								System.exit(0);
+					}
+					
+					break;
+				}
+				break;
+			}
+		PAST:
+			while(running)
+			{
+				System.out.println("You hear the door open and you stand up only to bump into the table.);
 				break;
 			}
 
